3b39b4de98764216e0dbedf5574398b21f620f6e
ThisIsHyum <em.appasov@gmail.com>
2025-11-15 20:47:57 +0500

    single-day getters now return single schedule

--- schedules.go
+++ schedules.go
@@ -2,6 +2,7 @@ package osago
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -10,16 +11,28 @@ import (
 	"github.com/ThisIsHyum/osago/types"
 )
 
-func (c *Client) GetSchedulesForToday(ctx context.Context, groupId uint) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "day=today")
+func (c *Client) GetScheduleForToday(ctx context.Context, groupId uint) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, "day=today")
 }
 
-func (c *Client) GetSchedulesForTomorrow(ctx context.Context, groupId uint) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "day=tomorrow")
+func (c *Client) GetScheduleForTomorrow(ctx context.Context, groupId uint) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, "day=tomorrow")
 }
 
-func (c *Client) GetSchedulesForDate(ctx context.Context, groupId uint, date time.Time) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "date="+url.QueryEscape(date.Format("02-01-2006")))
+func (c *Client) GetScheduleForDate(ctx context.Context, groupId uint, date time.Time) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, "date="+url.QueryEscape(date.Format("02-01-2006")))
+}
+
+func (c *Client) GetScheduleForWeekdayOfPreviousWeek(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=previous&weekday=%s", weekday))
+}
+
+func (c *Client) GetScheduleForWeekday(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=current&weekday=%s", weekday))
+}
+
+func (c *Client) GetScheduleForWeekdayOfNextWeek(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=next&weekday=%s", weekday))
 }
 
 func (c *Client) GetSchedulesForPreviousWeek(ctx context.Context, groupId uint) ([]types.Schedule, error) {
@@ -34,18 +47,6 @@ func (c *Client) GetSchedulesForNextWeek(ctx context.Context, groupId uint) ([]t
 	return c.getSchedules(ctx, groupId, "week=next")
 }
 
-func (c *Client) GetSchedulesForWeekdayOfPreviousWeek(ctx context.Context, groupId uint, weekday time.Weekday) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, fmt.Sprintf("week=previous&weekday=%s", weekday))
-}
-
-func (c *Client) GetSchedulesForWeekday(ctx context.Context, groupId uint, weekday time.Weekday) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, fmt.Sprintf("week=current&weekday=%s", weekday))
-}
-
-func (c *Client) GetSchedulesForWeekdayOfNextWeek(ctx context.Context, groupId uint, weekday time.Weekday) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, fmt.Sprintf("week=next&weekday=%s", weekday))
-}
-
 func (c *Client) getSchedules(ctx context.Context, groupId uint, query string) ([]types.Schedule, error) {
 	var schedules []types.Schedule
 	path := fmt.Sprintf("/groups/%d/schedules?%s", groupId, query)
@@ -53,4 +54,15 @@ func (c *Client) getSchedules(ctx context.Context, groupId uint, query string) (
 		return nil, err
 	}
 	return schedules, nil
+}
+
+func (c *Client) getSchedule(ctx context.Context, groupId uint, query string) (types.Schedule, error) {
+	schedules, err := c.getSchedules(ctx, groupId, query)
+	if err != nil {
+		return types.Schedule{}, err
+	}
+	if len(schedules) == 0 {
+		return types.Schedule{}, errors.New("schedule not found")
+	}
+	return schedules[0], nil
 }
