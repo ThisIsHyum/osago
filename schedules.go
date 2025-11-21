e6a3dc598cc5dd26768d30d8ceee42439f64c6af
ThisIsHyum <em.appasov@gmail.com>
2025-11-21 19:29:48 +0500

    rename groupId to groupID in schedules.go

--- schedules.go
+++ schedules.go
@@ -13,53 +13,53 @@ import (
 
 var ErrScheduleNotFound = errors.New("schedule not found")
 
-func (c *Client) GetScheduleForToday(ctx context.Context, groupId uint) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, "day=today")
+func (c *Client) GetScheduleForToday(ctx context.Context, groupID uint) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, "day=today")
 }
 
-func (c *Client) GetScheduleForTomorrow(ctx context.Context, groupId uint) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, "day=tomorrow")
+func (c *Client) GetScheduleForTomorrow(ctx context.Context, groupID uint) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, "day=tomorrow")
 }
 
-func (c *Client) GetScheduleForDate(ctx context.Context, groupId uint, date time.Time) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, "date="+url.QueryEscape(date.Format("02-01-2006")))
+func (c *Client) GetScheduleForDate(ctx context.Context, groupID uint, date time.Time) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, "date="+url.QueryEscape(date.Format("02-01-2006")))
 }
 
-func (c *Client) GetScheduleForWeekdayOfPreviousWeek(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=previous&weekday=%s", weekday))
+func (c *Client) GetScheduleForWeekdayOfPreviousWeek(ctx context.Context, groupID uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, fmt.Sprintf("week=previous&weekday=%s", weekday))
 }
 
-func (c *Client) GetScheduleForWeekday(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=current&weekday=%s", weekday))
+func (c *Client) GetScheduleForWeekday(ctx context.Context, groupID uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, fmt.Sprintf("week=current&weekday=%s", weekday))
 }
 
-func (c *Client) GetScheduleForWeekdayOfNextWeek(ctx context.Context, groupId uint, weekday time.Weekday) (types.Schedule, error) {
-	return c.getSchedule(ctx, groupId, fmt.Sprintf("week=next&weekday=%s", weekday))
+func (c *Client) GetScheduleForWeekdayOfNextWeek(ctx context.Context, groupID uint, weekday time.Weekday) (types.Schedule, error) {
+	return c.getSchedule(ctx, groupID, fmt.Sprintf("week=next&weekday=%s", weekday))
 }
 
-func (c *Client) GetSchedulesForPreviousWeek(ctx context.Context, groupId uint) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "week=previous")
+func (c *Client) GetSchedulesForPreviousWeek(ctx context.Context, groupID uint) ([]types.Schedule, error) {
+	return c.getSchedules(ctx, groupID, "week=previous")
 }
 
-func (c *Client) GetSchedulesForCurrentWeek(ctx context.Context, groupId uint) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "week=current")
+func (c *Client) GetSchedulesForCurrentWeek(ctx context.Context, groupID uint) ([]types.Schedule, error) {
+	return c.getSchedules(ctx, groupID, "week=current")
 }
 
-func (c *Client) GetSchedulesForNextWeek(ctx context.Context, groupId uint) ([]types.Schedule, error) {
-	return c.getSchedules(ctx, groupId, "week=next")
+func (c *Client) GetSchedulesForNextWeek(ctx context.Context, groupID uint) ([]types.Schedule, error) {
+	return c.getSchedules(ctx, groupID, "week=next")
 }
 
-func (c *Client) getSchedules(ctx context.Context, groupId uint, query string) ([]types.Schedule, error) {
+func (c *Client) getSchedules(ctx context.Context, groupID uint, query string) ([]types.Schedule, error) {
 	var schedules []types.Schedule
-	path := fmt.Sprintf("/groups/%d/schedules?%s", groupId, query)
+	path := fmt.Sprintf("/groups/%d/schedules?%s", groupID, query)
 	if err := c.doReq(ctx, http.MethodGet, path, nil, &schedules); err != nil {
 		return nil, err
 	}
 	return schedules, nil
 }
 
-func (c *Client) getSchedule(ctx context.Context, groupId uint, query string) (types.Schedule, error) {
-	schedules, err := c.getSchedules(ctx, groupId, query)
+func (c *Client) getSchedule(ctx context.Context, groupID uint, query string) (types.Schedule, error) {
+	schedules, err := c.getSchedules(ctx, groupID, query)
 	if err != nil {
 		return types.Schedule{}, err
 	}
