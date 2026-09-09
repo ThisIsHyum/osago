c1da893157b9ef4ad4c98ac7a5fbd9f0a982ad12
ThisIsHyum <108114255+ThisIsHyum@users.noreply.github.com>
2026-09-09 22:48:31 +0500

    fix: require scheme for client

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package osago
 import (
 	"errors"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/ThisIsHyum/osago/client"
@@ -15,8 +16,12 @@ type Client struct {
 	c *client.OpenScheduleAPI
 }
 
-func NewClient(url string, timeout time.Duration) *Client {
-	c := httptransport.NewWithClient(url, "", []string{"http", "https"}, &http.Client{
+func NewClient(URL string, timeout time.Duration) *Client {
+	u, err := url.Parse(URL)
+	if err != nil {
+		panic(err)
+	}
+	c := httptransport.NewWithClient(u.Host, u.Path, []string{u.Scheme}, &http.Client{
 		Timeout: timeout,
 	})
 	return &Client{c: client.New(c, nil)}
